6d32978b97e9f135e4f7f5f4d67987363a47c2c3
Jenna Claudia Andriamanantena <jennaandria@gmail.com>
2024-07-05 12:58:09 +0200

    Added back to lists

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,19 +1,13 @@
 class BookmarksController < ApplicationController
   def create
     @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list = @list
-    @bookmark.movie = Movie.find(params[:movie_id])
+    @movie = Movie.find(params[:movie_id])
+    @bookmark = Bookmark.new(list: @list, movie: @movie)
+
     if @bookmark.save
-      redirect_to list_path(@list)
+      redirect_to list_path(@list), notice: 'Movie was successfully added to the list.'
     else
-      render 'lists/show'
+      redirect_to list_path(@list), alert: 'There was an error adding the movie to the list.'
     end
-  end
-
-  private
-
-  def bookmark_params
-    params.permit(:comment)
   end
 end
